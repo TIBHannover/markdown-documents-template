d5cb17125fa0ae4f04a0c764e919bf26daf249af
mirjan-hoffmann <mirjan.hoffmann@tib.eu>
2023-05-16 11:40:37 +0200

    Add initialize repo workflow

---
+++ .init-repo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# adjust links in readme file
+sed -i "s,tibhannover.github.io/markdown-documents-template,${GITHUB_REPOSITORY_OWNER}.github.io/$REPO_NAME,g" README.md
+cat README.md
+
+# cleanup init-filees
+rm .init-repo.sh
+rm .github/workflows/initialize-repo.yml
